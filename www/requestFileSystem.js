9782197c3a5abce37b56f58a1e7b50a4b0523381
divyasakpal <divyasakpal14@gmail.com>
2016-12-12 15:16:33 +0530

    Update requestFileSystem.js
    Add path in native URL-   /d

--- www/requestFileSystem.js
+++ www/requestFileSystem.js
@@ -63,7 +63,7 @@
                         fileSystems.getFs(file_system.name, function(fs) {
                             // This should happen only on platforms that haven't implemented requestAllFileSystems (windows)
                             if (fs) {
-                                fs.root.nativeURL = "";
+                                fs.root.nativeURL = "/d";
                                // fs = new FileSystem(file_system.name, file_system.root);
                             }
                             successCallback(fs);
